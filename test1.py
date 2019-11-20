225e5dde27a396babc568ae0854a17b0c31bc451
Rasheed <Rasheeed@gmail.com>
2019-11-20 16:45:09 +0530

    second commit

--- test1.py
+++ test1.py
@@ -1,1 +1,17 @@
 line 1
+
+
+Question:  How can we revert a change which is already committed and pushed to git hub repository?
+Answer:
+"GIT revert" is used to revert the changes
+GIT push origin master
+If an automatic CI is deployed using Jenkins then there is a chance that code deployed is already deployed by Jenkins.
+
+Question:  Have you made mistakes and corrected?
+Answer:
+- Inconsistent code is pushed because of insufficient testing, reverted the code and
+- automate testing functionality to test code after every gitcommit
+- use Docker for same environment
+- use micro services to reduce the impact on  functionalities other than updated one
+- overcome risks to avoid failure before pushing the code
+
